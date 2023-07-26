8727e5fbeda6e743d5e61430143a211152240daf
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-07-26 14:15:46 -0400

    Outlined how website will be structured

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,9 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/*
+Navigation:
+  Logo 
+  Navigation bar
+
+MainHeading:
+  Image
+  Title
+  Paragraph + Button
+
+SideSubHeading:
+  Title
+  Subtitle + Paragraph
+  Subtitle + Paragraph
+  Subtitle + Paragraph
+
+BottomSubHeading
+  Image
+  Number + Title + Paragraph
+  Number + Title + Paragraph
+  Number + Title + Paragraph
+*/
+
 function App() {
   return (
     <div className="App">
+      <div className="Navigation">
+      </div>
+      <div className="MainHeading">
+      </div>
+      <div className="SideSubHeading">
+      </div>
+      <div className="BottomSubHeading">
+      </div>
     </div>
   );
 }
