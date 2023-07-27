53997fc9ccb579f29e1b6a8bc4d1418e6914c790
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-07-27 11:11:49 -0400

    Created SideSubHeading components

---
+++ app/src/components/SideSubHeading.tsx
@@ -0,0 +1,11 @@
+import './stylesheet/SideSubHeading.css';
+
+const SideSubHeading = () => {
+  return (
+    <div className="SideSubHeading">
+
+    </div> 
+  );
+};
+
+export default SideSubHeading;
