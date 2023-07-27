500f39a5d2a096319e2c9c866270346d7a9b2cd4
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-07-27 11:33:11 -0400

    Restyled MainHeading using flexboxes

--- app/src/components/MainHeading.tsx
+++ app/src/components/MainHeading.tsx
@@ -13,10 +13,12 @@ const MainHeading = () => {
   return (
     <div className="MainHeading">
       <img className="HeadingImage" src={headingImage} alt="image-web-3-desktop" />
-      <div className="HeadingTitle">{title}</div>
-      <div className="HeadingReadMore">
-        <div className="ReadMoreParagraph">{readmore}</div>
-        <button className="ReadMoreButton">read more</button>
+      <div className="Bottom">
+        <div className="HeadingTitle">{title}</div>
+        <div className="HeadingReadMore">
+          <div className="ReadMoreParagraph">{readmore}</div>
+          <button className="ReadMoreButton">read more</button>
+        </div>
       </div>
     </div>
   )
