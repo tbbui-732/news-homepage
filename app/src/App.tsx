3234b353f1c7829c0c32b88dbb0f7ed37d2f5e99
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-07-29 00:53:39 -0400

    Removed zombie comments

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -7,38 +7,6 @@ import MainHeading from "./components/MainHeading";
 import SideSubHeading from './components/SideSubHeading';
 import BottomSubHeading from './components/BottomSubHeading';
 
-/*
-Navigation:
-  Logo 
-  Navigation bar
-
-MainHeading:
-  Image
-  Title
-  Paragraph + Button
-
-SideSubHeading:
-  Title
-  Subtitle + Paragraph
-  Subtitle + Paragraph
-  Subtitle + Paragraph
-
-BottomSubHeading
-  Image
-  Number + Title + Paragraph
-  Number + Title + Paragraph
-  Number + Title + Paragraph
-*/
-
-// <div className="Navigation">
-//   <Navigation />
-// </div>
-// <div className="MainHeading">
-//   <MainHeading />
-// </div>
-// <div className="SideSubHeading"></div>
-// <div className="BottomSubHeading"></div>
-
 function App() {
   return (
     <div className="App">
