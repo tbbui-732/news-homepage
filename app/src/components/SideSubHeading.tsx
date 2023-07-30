d267eefc7d5346319feb16b244c67fa402c2e726
Thien-Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2023-07-29 23:37:44 -0400

    Converted sidesubheading divs into anchor tags for clickability

--- app/src/components/SideSubHeading.tsx
+++ app/src/components/SideSubHeading.tsx
@@ -4,28 +4,28 @@ const SideSubHeading = () => {
   return (
     <div className="SideSubHeading">
       <div className="Title">New</div>
-      <div className="NewsBlock">
+      <a href="#" className="NewsBlock">
         <div className="BlockHeading">Hydrogen VS Electric Cars</div>
         <div className="BlockSubHeading">
           Will hydrogen-fueled cars ever catch up to EVs?
         </div>
-      </div>
+      </a>
       <div className="LineBreak"></div>
-      <div className="NewsBlock">
+      <a href="#" className="NewsBlock">
         <div className="BlockHeading">The Downsides of AI Artistry</div>
         <div className="BlockSubHeading">
           What are the possible adverse effects of on-demand
           AI image generation?
         </div>
-      </div>
+      </a>
       <div className="LineBreak"></div>
-      <div className="NewsBlock">
+      <a href="#" className="NewsBlock">
         <div className="BlockHeading">Is VC Funding Drying Up?</div>
         <div className="BlockSubHeading">
           Private funding by VC firms is down 50% YOY.
           We take a look at what that means.
         </div>
-      </div>
+      </a>
     </div> 
   );
 };
